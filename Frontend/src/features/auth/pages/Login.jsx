91469513f0f56b538eda6327e4d410118187f051
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2026-08-26 17:06:17 +0530

    Auth Hook Added CORS added

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,17 +1,25 @@
-
-import {  Link } from 'react-router'
+import {useState} from 'react'
+import { useNavigate } from 'react-router'
 import "../auth.form.scss"
-
+import { useAuth } from '../hooks/useAuth'
 
 const Login = () => {
 
+    const { loading, handleLogin } = useAuth()
+    const navigate = useNavigate()
+
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-       
+        await handleLogin({email,password})
+        navigate('/')
     }
 
-  
+    if(loading){
+        return (<main><h1>Loading.......</h1></main>)
+    }
 
 
     return (
@@ -22,13 +30,13 @@ const Login = () => {
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
                         <input
-                           
+                            onChange={(e) => { setEmail(e.target.value) }}
                             type="email" id="email" name='email' placeholder='Enter email address' />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
                         <input
-                           
+                            onChange={(e) => { setPassword(e.target.value) }}
                             type="password" id="password" name='password' placeholder='Enter password' />
                     </div>
                     <button className='button primary-button' >Login</button>
