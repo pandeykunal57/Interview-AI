05e0fe0dad8bb2b6a4cbfa9c5dcd060bd74ffcac
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2026-08-26 17:32:18 +0530

    Protected Routes Added

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import { useNavigate } from 'react-router'
+import { useNavigate, Link } from 'react-router'
 import { useAuth } from '../hooks/useAuth'
 
 const Register = () => {
