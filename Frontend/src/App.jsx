c5bd00d717989b3d71a4a04463bd86525fe93db1
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2026-09-03 18:52:06 +0530

    Home and Interview Frontend Created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,20 +2,16 @@
 import { RouterProvider } from "react-router"
 import { router } from "./app.routes.jsx"
 import { AuthProvider } from "./features/auth/auth.Context.jsx"
-
-
-
+import { InterviewProvider } from "./features/interview/interview.context.jsx"
 
 function App() {
 
   return (
-
-  <AuthProvider>
-      <RouterProvider router={router} />
-  </AuthProvider>
-
-
-
+    <AuthProvider>
+      <InterviewProvider>
+        <RouterProvider router={router} />
+      </InterviewProvider>
+    </AuthProvider>
   )
 }
 
